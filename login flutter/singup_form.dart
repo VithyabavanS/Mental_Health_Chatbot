9b244cf510b8657e0d72f51fe8f87899fadac355
Shasindu Weerasekara <125337568+shasindu@users.noreply.github.com>
2023-07-23 13:06:17 +0530

    Update singup_form.dart

--- login flutter/singup_form.dart
+++ login flutter/singup_form.dart
@@ -2,5 +2,10 @@ class SignUpFormWidget extends StatelessWidget {
   const SignUpFormWidget({
     Key? key,
   }) : super(key: key);
+  
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(SignUpController());
+    final _formKey = GlobalKey<FormState>();
 
 
